9cce8d6250defdadf377cd1945469a8fe134a06b
Thorgardd <rouviller.jeremy59@gmail.com>
2023-01-24 16:35:10 +0100

    Push

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -7,27 +7,50 @@ const Details = () => {
 
     const ApiKey = "6eb21f772f8f904f7f8e43a3b60a13d8"
     const params = useParams();
-    const [movieId, setMovieId] = useState(params.id)
-    const [movie, setMovie] = useState({})
+    const [urlId, setUrlId] = useState(params.id)
+    const [object, setObject] = useState({})
+    const [isMovie, setIsMovie] = useState(false)
 
     useEffect(() => {
-        GetMovieById(movieId)
+        if (CheckIsMovieParams()){
+            GetMovieById(urlId)
+        } else if (!CheckIsMovieParams()){
+            GetShowById(urlId)
+        }
     }, [])
-    
+
+    const CheckIsMovieParams = () => {
+        if (window.location.href.indexOf("movies") !== -1){
+            setIsMovie(true);
+            return true;
+        } else {
+            setIsMovie(false);
+            return false;
+        }
+    }
+
     const GetMovieById = (id) => {
         axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${ApiKey}&language=en-US`)
             .then((res) => {
-                setMovie(res.data)
+                setObject(res.data)
             })
             .catch((err) => console.log(err))
     }
 
+    const GetShowById = (id) => {
+        axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${ApiKey}&language=en-US`)
+            .then((res) => {
+                setObject(res.data)
+            })
+            .catch((err) => console.log(err));
+    }
+
     return (
         <div>
-            <Infos>{movie.id}</Infos>
-            <Infos>{movie.overview}</Infos>
-            <Infos>{movie.title}</Infos>
-            <Infos>{movie.vote_count}</Infos>
+            <Infos>{object.id}</Infos>
+            <Infos>{object.overview}</Infos>
+            <Infos>{object.title}</Infos>
+            <Infos>{object.vote_count}</Infos>
         </div>
     );
 };
